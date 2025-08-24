f84527d83109c0fd20021f0f6f78753cc362d9e5
FriedS0ul <yaroslav.khomyakov@gmail.com>
2025-08-24 08:48:40 +0500

    started

---
+++ keyboards/friedsoul/ec_macropad/config.h
@@ -0,0 +1,32 @@
+
+
+#pragma once
+
+// MATRIX 
+#define Matrix_rows
+#define Matrix_cols
+
+#define Matrix_rows_pins
+#define Matrix_cols_pins
+
+// AMUX
+#define Amux_channel_max
+#define Amux_channel_in_use
+
+#define Amux_select_pins
+#define Amux_enable_pins
+
+// ANALOG PINS
+#define Discharge_pin
+#define Analog_input_pin
+
+
+
+//NUMBES
+
+#define Bottom_level
+#define Top_level
+#define Noise_level
+
+#define Actuation_level
+#define Release_level
