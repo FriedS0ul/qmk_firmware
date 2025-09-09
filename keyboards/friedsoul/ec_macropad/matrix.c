51ebf6e6106127b94a70ff870ddc03f6e6252fbb
FriedSoul <yaroslav.khomyakov@gmail.com>
2025-09-09 19:23:43 +0500

    inits

--- keyboards/friedsoul/ec_macropad/matrix.c
+++ keyboards/friedsoul/ec_macropad/matrix.c
@@ -16,15 +16,26 @@ extern matrix_row_t matrix[MATRIX_ROWS];
 void adc_init(void){
 
     ADMUX = (1 << REFS0); // Опорное напряжение == Vcc
-    DIDR0 |= (1 << ADC0D); // Отключение цифрогово входа на входном пине (F0)
     ADCSRA |= (1 << ADPS0) | (1 << ADPS1) | (1 << ADPS2); // Делитель 128 для установки частоты ацп 125Кгц
     ADCSRA |= (1 << ADEN); // Включение АЦП
+
+}
+
+// Инициализация пинов (вход - выход)
+void pins_init(void){
+
+    DIDR0 |= (1 << ADC0D); // Отключение цифрогово входа на входном пине F0
+    DDRF &= ~(1 << DDF0); // Сброс значения F0 (становится входом)
+    DDRF |= (1 << DDF4) | (1 << DDF5) | (1 << DDF6); // Установка упрявляющих пинов мультиплексора как выходов
+    DDRD = (1 << DDD4) | (1 << DDD6) | (1 << DDD7); // Установка рядов матрицы как выходов (0, 1, 2)
 
 }
 
 
 // Initialize hardware here
 void matrix_init_custom(void){
+
+    pins_init();
     adc_init();
 
     gpio_write_pin_low(AMUX_EN_PINS);
@@ -41,6 +52,8 @@ void matrix_init_custom(void){
 
 //Matrix scanning process
 bool matrix_scan_custom(matrix_row_t current_matrix[]){
+    
+    bool matrix_has_changed = false;
 
 
 
