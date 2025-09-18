5282a1a9a563e06a6d615bf85682eb4ecfc1762c
FriedSoul <yaroslav.khomyakov@gmail.com>
2025-09-18 15:47:23 +0500

    rewritten to use qmk functions instead of bits

--- keyboards/friedsoul/ec_macropad/matrix.c
+++ keyboards/friedsoul/ec_macropad/matrix.c
@@ -10,8 +10,12 @@
 #include <print.h>
 
 
-const pin_t row_pins[] = MATRIX_ROWS_PINS;
 
+
+const pin_t row_pins[] = MATRIX_ROWS_PINS;
+const pin_t amux_sel[] = AMUX_SEL_PINS;
+
+static adc_mux adcMux;
 
 
 // Инициализация АЦП
@@ -20,81 +24,82 @@ void adc_init(void){
     ADMUX = (1 << REFS0); // Опорное напряжение == Vcc
     ADCSRA |= (1 << ADPS0) | (1 << ADPS1) | (1 << ADPS2); // Делитель 128 для установки частоты ацп 125Кгц
     ADCSRA |= (1 << ADEN); // Включение АЦП
+    DIDR0 |= (1 << ADC0D); // Отключение цифрогово входа на входном пине F0
 
 }
 
 // Инициализация пинов (вход - выход)
 void pins_init(void){
 
-    DIDR0 |= (1 << ADC0D); // Отключение цифрогово входа на входном пине F0
-    //DDRF &= ~(1 << DDF0); 
-    gpio_set_pin_input(F0); // Сброс значения F0 (становится входом)
-    DDRF |= (1 << DDF4) | (1 << DDF5) | (1 << DDF6); // Установка упрявляющих пинов мультиплексора как выходов
-    DDRD = (1 << DDD4) | (1 << DDD6) | (1 << DDD7); // Установка рядов матрицы как выходов (0, 1, 2)
+    gpio_set_pin_input(ANALOG_READINGS_INPUT); // Аналоговый пин становится входом
+    gpio_set_pin_output(AMUX_EN_PINS); // AMUX_EN становится выходом
+    gpio_write_pin_high(AMUX_EN_PINS); // Выключение мультиплексора
+    gpio_write_pin_low(DISCHARGE_PIN); // Инициализация пина для разрядки ряда
+    gpio_set_pin_output(DISCHARGE_PIN); // Инициализация пина для разрядки ряда
 
+    for (uint8_t i = 0; i < (sizeof(amux_sel) / sizeof(amux_sel[0])); i++) // Выставляем управлящие пины мультиплексора как выходы + low
+    {
+        gpio_set_pin_output(amux_sel[i]);
+        gpio_write_pin_low(amux_sel[i]);
+    }
+
+    for (uint8_t i = 0; i < (sizeof(row_pins) / sizeof(row_pins[0])); i++) // Выставляем управлящие пины ряды матрицы как выходы + low
+    {
+        gpio_set_pin_output(row_pins[i]);
+        gpio_write_pin_low(row_pins[i]); 
+    }
 }
 
+// Функция зарядки ряда
+void row_charge(pin_t ){
 
-// Initialize hardware here
+    gpio_set_pin_input(DISCHARGE_PIN); // Отключаем пин разрядки
+    gpio_write_pin_high(pin); // Включаем зарядку
+    wait_us(50);
+}
+
+// Функция разрядки ряда
+void row_discharge(void){
+
+    gpio_set_pin_output(DISCHARGE_PIN);
+    gpio_write_pin_low(DISCHARGE_PIN);
+}
+
+// Инициализация при старте (СТАНДАРТНАЯ ФУНКЦИЯ)
 void matrix_init_custom(void){
-
+    
+    adcMux = pinToMux(ANALOG_READINGS_INPUT);
     pins_init();
     adc_init();
-
 }
 
 
-//Matrix scanning process
+// Скан матрицы (СТАНДАРТНАЯ ФУНКЦИЯ)
 bool matrix_scan_custom(matrix_row_t current_matrix[]){
     bool matrix_has_changed = true;
 
-    gpio_write_pin_low(AMUX_EN_PINS); // Включаем мультиплексор и выставляем каналы в 0 0 0
-
-    gpio_write_pin_low(F4);
-    gpio_write_pin_low(F5);
-    gpio_write_pin_low(D4);
-    
-
     for (uint8_t row = 0; row < (sizeof(row_pins) / sizeof(row_pins[0])); row++)
     {
-        gpio_set_pin_output(row_pins[row]); // Ставим пин как выход
-        gpio_write_pin_high(row_pins[row]); // Включаем зарядку ряда
-
-        wait_us(100); // Ждем зарядку ряда
-
-        gpio_write_pin_low(row_pins[row]); // Отключаем зарядку ряда
-
-        wait_us(10);
+        row_charge(row_pins[row]); // Включаем зарядку ряда
+        gpio_write_pin_low(AMUX_EN_PINS); // Включаем мультиплексор
         
         for (uint8_t col = 0; col < AMUX_CHANNELS_OCCUPIED; col++)
         { 
             gpio_write_pin(F4, (col >> 0) & 1); // Извлекаем бит и устанавливаем пин high или low
             gpio_write_pin(F5, (col >> 1) & 1);
             gpio_write_pin(D4, (col >> 2) & 1);
-
-            ADCSRA |= (1 << ADSC); // Включение чтения ацп
-
-            while (ADCSRA & (1 << ADSC))
-            {
-                // Ждем автоматического завершения чтения
-            }
-
-            uint16_t adc_readings = ADC; // Смотрим прочитанное значение
-            uprintf("Row %d, Col %d: %u\r\n", row, col, adc_readings);
-
             
+            uint16_t adc_readings = adc_read(adcMux); // Смотрим прочитанное значение
+
+            uprintf("Row %d, Col %d: %u\r\n", row, col, adc_readings); // Выводим полученные значения в HID консоль
         }
 
-        gpio_set_pin_input(row_pins[row]); // Ставим пин как вход
+        gpio_write_pin_high(AMUX_EN_PINS); // Отключаем мультиплексор 
 
-        wait_ms(50); // Снижаем частоту опроса для тестов
+        row_discharge();
 
+        wait_ms(20); // Снижаем частоту опроса для тестов
     }
     
-
-    
-
-    
     return matrix_has_changed;
-
 }
