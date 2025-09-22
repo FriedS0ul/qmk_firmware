ddf88d60478e6d44b5f25aa190c6c1a949667a1e
FriedS0ul <yaroslav.khomyakov@gmail.com>
2025-09-23 01:59:08 +0500

    Ghosting solved ? rows doesnt appear to be switching ?

--- keyboards/friedsoul/ec_macropad/matrix.c
+++ keyboards/friedsoul/ec_macropad/matrix.c
@@ -33,7 +33,7 @@ void pins_init(void){
     for (uint8_t i = 0; i < (sizeof(row_pins) / sizeof(row_pins[0])); i++) // Выставляем управлящие пины ряды матрицы как выходы + low
     {
         gpio_set_pin_output(row_pins[i]);
-        gpio_write_pin_low(row_pins[i]); 
+        gpio_write_pin_high(row_pins[i]); 
     }
 
     for (uint8_t i = 0; i < (sizeof(amux_sel) / sizeof(amux_sel[0])); i++) // Выставляем управлящие пины мультиплексора как выходы + low
@@ -44,10 +44,10 @@ void pins_init(void){
 }
 
 // Функция зарядки ряда
-void row_charge(pin_t pin)
+void row_charge(uint8_t pin)
 {
     gpio_set_pin_input(DISCHARGE_PIN);
-    gpio_write_pin_high(pin);
+    gpio_write_pin_high(row_pins[pin]);
     wait_us(CHARGE_TIME_US);
 }
 
@@ -62,46 +62,37 @@ void pin_discharge(void)
 // Функция для выбора каналов мультиплексора
 void mux_channel_select(uint8_t col) 
 {
+    gpio_write_pin_high(AMUX_EN_PINS);
     for (uint8_t i = 0; i < (sizeof(amux_sel) / sizeof(amux_sel[0])); i++) // Выбираем значение low или high на основе номера нужной колонки
     {
         gpio_write_pin(amux_sel[i], (col >> i) & 1);
     }
+    wait_us(10);
+    gpio_write_pin_low(AMUX_EN_PINS);
 }
 
 // Сканирование RAW значений с конкретного датчика по адресу в матрице
 uint16_t ec_sw_scan_raw(uint8_t col, uint8_t row)
 {
-    gpio_write_pin_high(AMUX_EN_PINS);
-    mux_channel_select(col); // MUX не включается, только ставится канал
-
-    gpio_set_pin_output(row_pins[row]);
     gpio_write_pin_low(row_pins[row]);
-
     wait_us(DISCHARGE_TIME_US);
 
+
     gpio_set_pin_input(DISCHARGE_PIN);
-
-
     gpio_write_pin_high(row_pins[row]);
-    
     wait_us(CHARGE_TIME_US);
 
-    gpio_write_pin_low(AMUX_EN_PINS); // MUX вкл
-
-    wait_us(10);
 
     uint16_t raw_adc_readings = analogReadPin(ANALOG_READINGS_INPUT);
 
-    gpio_set_pin_input(row_pins[row]);
 
     gpio_write_pin_high(AMUX_EN_PINS); // MUX выкл
-
-    gpio_write_pin_low(DISCHARGE_PIN);
     gpio_set_pin_output(DISCHARGE_PIN);
+
+    uprintf("ROW %d, COL %d: %u\r\n", row, col, raw_adc_readings); // Выводим полученные значения в HID консоль
 
     wait_us(DISCHARGE_TIME_US);
 
-    uprintf("COL %d, ROW %d: %u\r\n", col, row, raw_adc_readings); // Выводим полученные значения в HID консоль
     return raw_adc_readings;
 }
 
@@ -120,6 +111,7 @@ void ec_matrix_noise_sample(void)
     {
         for (uint8_t col = 0; col < MATRIX_COLS; col++)
         {
+            mux_channel_select(col);
             for (uint8_t row = 0; row < MATRIX_ROWS; row++)
             {
                 ec_noise_threshold[col][row] += ec_sw_scan_raw(col, row);
@@ -135,6 +127,7 @@ void ec_matrix_noise_sample(void)
             ec_noise_threshold[col][row] += NOISE_OFFSET;
         } 
     }
+     uprintf("Sampling done 0,0 threshold: %u\n", ec_noise_threshold[0][0]);
 }
 
 //Сканирование матрицы и обновление current_matrix
@@ -143,16 +136,13 @@ bool ec_matrix_scan(matrix_row_t current_matrix[])
     bool matrix_has_changed = false;
     for (uint8_t col = 0; col < MATRIX_COLS; col++)
     {
+        mux_channel_select(col);
         for (uint8_t row = 0; row < MATRIX_ROWS; row++)
         {
             uint16_t raw_adc_readings = ec_sw_scan_raw(col, row);
             uint8_t previous_state = (current_matrix[row] >> col) & 1;
 
-            if (raw_adc_readings < ec_noise_threshold[col][row]) // Сравниваем
-            {
-                // Ничего не делаем
-            }
-            else if (raw_adc_readings < RELEASE_LEVEL && previous_state == 1) 
+            if (raw_adc_readings < RELEASE_LEVEL && previous_state == 1) 
             {
                 current_matrix[row] &= ~(1 << col); // Отпускаем
                 matrix_has_changed = true;
