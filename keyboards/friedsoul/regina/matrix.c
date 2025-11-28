e54b3072765f9c6e27bb4762622ddac1b9dd96d6
FriedSoul <yaroslav.khomyakov@gmail.com>
2025-11-28 16:32:55 +0500

    ITS ALIVE

--- keyboards/friedsoul/regina/matrix.c
+++ keyboards/friedsoul/regina/matrix.c
@@ -8,7 +8,7 @@
 
 const pin_t row_pins [] = MATRIX_ROW_PINS;
 uint16_t log_matrix[MATRIX_COLS][MATRIX_ROWS];
-uint8_t scan_counter = 0;
+uint16_t scan_counter = 0;
 
 
 void adc_int(void){
@@ -19,32 +19,29 @@ void adc_int(void){
 
 void pins_init(void){
 
-    gpio_set_pin_input(ANALOG_READINGS_INPUT);
-
     gpio_write_pin_low(DISCHARGE_PIN);
     gpio_set_pin_output(DISCHARGE_PIN);
     
 
     for (uint8_t i = 0; i < MATRIX_ROWS; i++)
     {
-        gpio_write_pin_high(row_pins[i]);
+        gpio_write_pin_low(row_pins[i]);
         gpio_set_pin_output(row_pins[i]);
         
     }
     
 }
- // Вывод сканирования в консоль каждые 250 циклов
+ // Вывод сканирования в консоль каждые 500 циклов
  void logger(void){
-    if (scan_counter == 250)
+    if (scan_counter == 500)
     {
-        uprintf("\n");
-        uprintf("\n");
+        uprintf("\r\n");
         for (uint8_t col = 0; col < MATRIX_COLS; col++)
         {
             for (uint8_t row = 0; row < MATRIX_ROWS; row++)
             {
                 uprintf("COL %d ROW %d: %u", col, row, log_matrix[col][row]);
-                uprintf("\n");
+                uprintf("\r\n");
             }
         }
         scan_counter = 0;
@@ -56,57 +53,68 @@ void pins_init(void){
  }
 
  // Сканирование конкретного датчика по адресу в матрице
- uint16_t ec_sw_scan(pin_t col, pin_t row){
+ uint16_t ec_sw_scan(uint8_t col, uint8_t row){
 
     uint16_t raw_adc_readings = 0;
 
-    gpio_write_pin_low(DISCHARGE_PIN);
+    gpio_write_pin_low(row_pins[row]);
+    gpio_set_pin_output(row_pins[row]);
+
     gpio_set_pin_input(DISCHARGE_PIN);
+    gpio_write_pin_low(DISCHARGE_PIN);
 
-    gpio_write_pin_low(row);
-    gpio_set_pin_output(row);
+    gpio_write_pin_high(row_pins[row]);
+    gpio_set_pin_output(row_pins[row]);
 
-    ATOMIC_BLOCK_FORCEON
-    {
-        gpio_write_pin_high(row);
-        gpio_set_pin_output(row);
-
-        raw_adc_readings = analogReadPin(ANALOG_READINGS_INPUT);
-    }
-
-    log_matrix[col][row] = raw_adc_readings;
+    raw_adc_readings = analogReadPin(ANALOG_READINGS_INPUT);
 
     gpio_write_pin_low(DISCHARGE_PIN);
     gpio_set_pin_output(DISCHARGE_PIN);
 
-    wait_us(50);
+    wait_us(DISCHARGE_TIME_US);
 
-    //uprintf("something %u\r\n", raw_adc_readings);
+    log_matrix[col][row] = raw_adc_readings; // Логирования сканирований
 
     return raw_adc_readings;
  }
-
-
-
-void matrix_init_custom(void) {
-    adc_int();
-    pins_init();
-
-    uprintf("initialization complete\r\n"); 
-}
-
-bool matrix_scan_custom(matrix_row_t current_matrix[]) {
-    bool matrix_has_changed = true;
-
-    logger();
+ // Функция сканирования и обновления current matrix
+ bool ec_matrix_scan(matrix_row_t current_matrix[]){
+    bool has_changed = false;
 
     for (uint8_t col = 0; col < MATRIX_COLS; col++)
     {
         for (uint8_t row = 0; row < MATRIX_ROWS; row++)
         {
-            ec_sw_scan(col, row);
+            uint16_t raw_adc_readings = ec_sw_scan(col, row); // Получаем данные сканирования конкретного датчика
+            uint8_t key_previous_state = (current_matrix[row] >> col) & 1; // Запрашиваем текущее состояние клавиши (нажата или отпущена)
+
+            if (raw_adc_readings > ACTUATION_LEVEL && key_previous_state == 0)
+            {
+                current_matrix[row] |= (1 << col); // Нажимаем
+                has_changed = true;
+            } else if (raw_adc_readings < RELEASE_LEVEL && key_previous_state == 1)
+            {
+               current_matrix[row] &= ~(1 << col); // Отпускаем
+               has_changed = true;
+            }
         }
-        
     }
+    return has_changed;
+ }
+
+
+
+void matrix_init_custom(void) {
+
+    adc_int();
+    pins_init();
+
+}
+
+bool matrix_scan_custom(matrix_row_t current_matrix[]) {
+    bool matrix_has_changed = ec_matrix_scan(current_matrix);
+
+    logger();
+
     return matrix_has_changed;
 }
